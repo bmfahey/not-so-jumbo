4532ca6d7a4ef566ba4766eea21bb8cbf38a5f4c
Brendan <bmfahey97@gmail.com>
2016-03-26 00:34:17 -0400

    fixed path

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(request, response) {
-  response.sendFile(path.join(__dirname, './', 'index.html'));
+  response.sendFile('./index.html' , { root : __dirname});
 });
 
 app.listen(process.env.PORT || 8888);
