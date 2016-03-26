4f0ec6ef29af1d5867981a90421bda1cf57f5144
Brendan <bmfahey97@gmail.com>
2016-03-26 00:28:22 -0400

    changed filepath

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var app = express();
 app.set('port', (process.env.PORT || 5000));
 
 app.get('/', function(request, response) {
-  response.send('./index.html');
+  response.sendFile(path.join(__dirname, './', 'index.html'));
 });
 
 app.listen(process.env.PORT || 8888);
