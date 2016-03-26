ef135b12a7bd41db3f839c1276d65378de2d46f4
Brendan <bmfahey97@gmail.com>
2016-03-26 00:31:07 -0400

    modified index.js

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 var express = require('express');
 var app = express();
 
-app.set('port', (process.env.PORT || 5000));
-
 app.get('/', function(request, response) {
   response.sendFile(path.join(__dirname, './', 'index.html'));
 });
