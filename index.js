0ca9605b48baa2e180801c84c3e5bce872ed2d8f
Brendan <bmfahey97@gmail.com>
2016-03-26 01:08:16 -0400

    sending less files

--- index.js
+++ index.js
@@ -7,8 +7,6 @@ app.use('./', function (req, res, next) {
 
 app.get('/', function(request, response) {
   response.sendFile('./index.html' , { root : __dirname});
-  response.sendFile('./images/elepant.png', {root : __dirname});
-  response.sendFile('./style.css', {root: __dirname});
 });
 
 app.listen(process.env.PORT || 8888);
