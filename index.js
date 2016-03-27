c235e269755a6bc7c86467127aa59a2d1c4f7d99
Brendan <bmfahey97@gmail.com>
2016-03-27 14:44:10 -0400

    changed server requests

--- index.js
+++ index.js
@@ -4,7 +4,19 @@ var app = express();
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(request, response) {
-  response.sendFile(__dirname + "/index.html");
+  response.sendFile(__dirname + "./index.html");
 });
+
+app.get('./progress', function (request, response) {
+	response.sendFile(__dirname + "./progress.html");
+});
+
+app.get('./goal', function(request,response) {
+	response.sendFile(__dirname+"./goal.html");
+});
+
+app.get('./dining', function(request,response) {
+	reponse.sendFile(__dirname+"./goal.html");
+})
 
 app.listen(process.env.PORT || 8888);
