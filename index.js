8608b94a811f5880079daf92999943d49cdb7baf
chendonna <chendonna@gmail.com>
2016-04-08 14:20:52 -0400

    updated package.json and index.js

--- index.js
+++ index.js
@@ -1,10 +1,37 @@
 var express = require('express');
+
+var bodyParser = require('body-parser');
+var validator = require('validator');
+
 var app = express();
 var router = express.Router();
 var path = __dirname + "/public/";
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+var mongoUri = process.env.MONGOLAB_URI || process.env.MONGOHQ_URL;
+var MongoClient = require('mongodb').MongoClient, format = require('util').format;
+var db = MongoClient.connect(mongoUri, function (error, databaseConnection) {
+	db = databaseConnection;
+})
 app.use(express.static(path));
 
+app.post('/submitFood', function(request, response) {
+	var id = request.body.id;
+	var timeStamp = request.body.timeStamp;
+	var protein = request.body.protein;
+	var fat = request.body.fat;
+	var calories = request.body.calories;
+	var toInsert = {
+		"id": id,
+		"timeStamp": timeStamp,
+		"protein": protein,
+		"fat": fat,
+		"calories": calories,
+	}
+	
+});
 router.use(function (req,res,next) {
 	next();
 });
