9217fd64d78b73b69d5ebd40586dd4f6efd122c8
Brendan <bmfahey97@gmail.com>
2016-04-20 15:25:29 -0400

    changed result to result[0]

--- index.js
+++ index.js
@@ -90,7 +90,7 @@ function initPerson(fb_id, protein, calories, fat, dow) {
 						result[0].days[dow].fat = currentFat;
 						result[0].days[dow].protein = currentProtein;
 						result[0].days[dow].calories = currentCalories;
-						coll.update({"FB_id":fb_id}, {$set: result}, function(error, result) {
+						coll.update({"FB_id":fb_id}, {$set: result[0]}, function(error, result) {
 							if (error) {
 								response.send(500);
 							} else {
