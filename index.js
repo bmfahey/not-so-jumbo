26b64724cf18aa705c0bfafbfe7ee63762bff494
Brendan <bmfahey97@gmail.com>
2016-04-20 15:08:39 -0400

    trying with new account

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ app.post('/submitFood', function(request, response) {
 					result[0].days[dow].fat = currentFat;
 					result[0].days[dow].protein = currentProtein;
 					result[0].days[dow].calories = currentCalories.
-					db.collection('users').update({"FB_id":fb_id}, {$set: result[0]}, function(error, result) {
+					coll.update({"FB_id":fb_id}, {$set: result[0]}, function(error, result) {
 						if (error) {
 							response.send(500);
 						} else {
