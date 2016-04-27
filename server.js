ac06b9870fe67980ea5706c4fa1bcea7a9f43b68
Harrison <uwaces@gmail.com>
2016-04-27 16:00:13 -0400

    Added cronjobs

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var validator = require('validator');
 
+var crontab = require('node-crontab');
+
 var app = express();
 
 var path = __dirname + "/public/";
@@ -15,6 +17,16 @@ var MongoClient = require('mongodb').MongoClient, format = require('util').forma
 var db = MongoClient.connect(mongoUri, function (error, databaseConnection) {
 	db = databaseConnection;
 })
+
+
+var job0 = crontab.scheduleJob("1 0 * * 0", deleteThisDay(0));
+var job1 = crontab.scheduleJob("1 0 * * 1", deleteThisDay(1));
+var job2 = crontab.scheduleJob("1 0 * * 2", deleteThisDay(2));
+var job3 = crontab.scheduleJob("1 0 * * 3", deleteThisDay(3));
+var job4 = crontab.scheduleJob("1 0 * * 4", deleteThisDay(4));
+var job5 = crontab.scheduleJob("1 0 * * 5", deleteThisDay(5));
+var job6 = crontab.scheduleJob("1 0 * * 6", deleteThisDay(6));
+
 app.use(express.static(path));
 
 app.post('/submitFood', function(request, response) {
@@ -224,5 +236,24 @@ router.get("/dining", function(request,response) {
 });
 
 app.use("/",router);
+
+deleteThisDay(day){
+        db.collection('users', function(error, coll) {
+                db.collection('users').find().toArray(function (error, result) {
+                        for(i = 0; i < result.length; i++){
+                                result[i].days[day].fat = 0;
+                                result[i].days[day].protein = 0;
+                                result[i].days[day].calories = 0;
+                                db.collection('users').update({"FB_id":fb_id}, {$set: result[0]}, function(error, result) {
+                                        if (error) {
+                                                response.send(500);
+                                        } else {
+                                                response.send(200);
+                                        }
+                                });
+                        }
+                }); //if user does not exist
+        });
+}
 
 app.listen(process.env.PORT || 8888);
