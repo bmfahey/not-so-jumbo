7f05b9e3a499a593a7acee14b3868d3b312f7cb5
Brendan <bmfahey97@gmail.com>
2016-04-30 17:40:28 -0400

    fixed some corner cases with data

--- public/javascript/progress.js
+++ public/javascript/progress.js
@@ -63,6 +63,9 @@ function login_success() {
                 alert("Sorry, that didn't submit! Try again.");
             },
             success: function(result) {
+                if (result == {} || (result.fat_goal == "" && result.pro_goal == "" && result.cal_goal == "")) {
+                    $("#goal-display").val("Something went wrong! Make sure you are logged into Facebook and have set a goal.");
+                }
                 var fat = parseInt(parseFloat(result.fat) * 100);
                 var protein = parseInt(parseFloat(result.protein) * 100);
                 var calories = parseInt(parseFloat(result.calories) * 100);
